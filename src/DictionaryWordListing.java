3d631560e009c39f9bbc9e79863b8eb5e9a940b9
azdoolittle <drewdoolittle@gmail.com>
2020-04-17 15:56:38 -0400

    working bubblesort both ways

--- src/DictionaryWordListing.java
+++ src/DictionaryWordListing.java
@@ -5,21 +5,31 @@ import Arrays
 
 declare class
 declare main method
-declare method to print original list
-declare method to print ascending order method
-declare method to print descending method
-declare vars for words 1-10
+declare string array for words 1-10
 declare scanner
 
-initialize methods
-
 prompt user to enter 10 words
-initialize each array index as input word
+initialize each array index as input word using for loop
 
-prompt user to use a method
+prompt user how to display dictionary
+use switch case to determine next step
+0 quit
+1 original
+2 abc
+3 zyx
 
-
+declare method to print original list
+declare bubble sort method
+    bubble sort takes in list and order type
+    create first loop to check once
+    create second loop to check twice
+    depending on order type
+    if word1 compare to word2 is neg or pos
+    use a temp variable to hold one value
+    switch pointers and values around
+print dictionary
  */
+
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -28,6 +38,7 @@ public class DictionaryWordListing {
             int userInput;
             String[] tenWords = new String[10];
             Scanner inputWord = new Scanner(System.in);
+
             System.out.println("Let's make a dictionary! Give me 10 words.");
             System.out.println("Enter a word (1/10): ");
             tenWords[0] = inputWord.nextLine();
@@ -36,8 +47,6 @@ public class DictionaryWordListing {
                 System.out.printf("Enter a word (%d/10): ", i + 1);
                 tenWords[i] = inputWord.nextLine();
             }
-//            inputWord.nextLine();
-
 
             System.out.println("How would you like to view your dictionary?\n" +
                     "Enter '0' to quit.\n" +
@@ -63,8 +72,6 @@ public class DictionaryWordListing {
                     bubbleSort(tenWords, "zyx");
                     break;
         }
-
-
     }
     public static void original(String[] list) {
         System.out.println(Arrays.toString(list));
@@ -75,28 +82,25 @@ public class DictionaryWordListing {
         int length = list.length;
         String nextWord, thisWord;
 
-        for (int i = 0; i < list.length - 1; i++) {
-            for (int k= 0; k < list.length - i - 1; k++) {
-                nextWord = list[k + 1];
-                thisWord = list[k];
-                if (order.equals("abc")) {
-                    if (nextWord.compareTo(thisWord) < 0) {
-                        temp = thisWord;
-                        list[k] = nextWord;
-                        list[k + 1] = temp;
-                    }
-                } else {
-                    if (thisWord.compareTo(nextWord) < 0) {
-                        temp = nextWord;
-                        list[k] = thisWord;
-                        list[k + 1] = temp;
+            for (int i = 0; i < length - 1; i++) {
+                for (int k = 0; k < length - i - 1; k++) {
+                    thisWord = list[k];
+                    nextWord = list[k + 1];
+                    if (order.equals("abc")) {
+                        if (nextWord.compareTo(thisWord) < 0) {
+                            temp = thisWord;
+                            list[k] = nextWord;
+                            list[k + 1] = temp;
+                        }
+                    } else {
+                        if (nextWord.compareTo(thisWord) > 0) {
+                            temp = thisWord;
+                            list[k] = nextWord;
+                            list[k + 1] = temp;
+                        }
                     }
                 }
-
             }
-        }
         System.out.println(Arrays.toString(list));
     }
-
-
 }
